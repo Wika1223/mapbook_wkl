fca11f5c937b84de7960d57eabcccbba08033883
Wika1223 <u73515@student.wat.edu.pl>
2026-04-30 12:40:02 +0200

    feat: add insert funcionality

--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -2,3 +2,11 @@ def read_data(users_data: list) -> None:
     for user in users_data:
         print(
             f'Twój znajomy {user['username']} z miejscowości {user["location"]} opublikował {user['posts']} wiadomości. Ostatnia wiadomość: {user["usermessage"][-1]}.')
+
+def add_user(users_data: list)-> None:
+    name = input('Podaj Imie: ')
+    location = input('Podaj lokalizacje: ')
+    posts = int(input('Podaj ilość postów: '))
+    usermessage = ['']
+    users_data.append({"username": name, 'location': location, 'posts': posts,
+         'usermessage': usermessage})
